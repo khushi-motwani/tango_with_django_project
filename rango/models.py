2aa442e37d0fcbac80afe87bfc40ea9c5666655e
2796649m@student.gla.ac.uk <2796649m@student.gla.ac.uk>
2024-01-30 13:38:37 +0000

    Chapter 5

--- rango/models.py
+++ rango/models.py
@@ -1,8 +1,11 @@
+from email.policy import default
 from tabnanny import verbose
 from django.db import models
 
 class Category(models.Model):
     name = models.CharField(max_length=128, unique=True)
+    views = models.IntegerField(default=0)
+    likes = models.IntegerField(default=0)
 
     class Meta:
         verbose_name_plural = 'Categories'
