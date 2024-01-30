016df57f0491ed46b14f64c467b4ebb9148b4821
2796649m@student.gla.ac.uk <2796649m@student.gla.ac.uk>
2024-01-30 13:00:47 +0000

    Chapter 5 w/o Exercises

--- rango/admin.py
+++ rango/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
+#suppose to be an include statement here? page 85
 
-# Register your models here.
+from django.contrib import admin
+from rango.models import Category, Page
+
+admin.site.register(Category)
+admin.site.register(Page)
+
